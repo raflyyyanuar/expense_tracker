a1d48926aa71a9868cbc176b0df20d6da210cffd
Rafly Yanuar <muhammad.rafly02@ui.ac.id>
2024-10-18 20:20:48 +0700

    added dummy expenses

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,4 @@
-
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -11,6 +11,39 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> registeredExpenses = [
+    Expense(
+      title: "Gym",
+      amount: 14.99,
+      date: DateTime.now(),
+      category: Category.LEISURE,
+    ),
+    Expense(
+      title: "Netflix",
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.LEISURE,
+    ),
+    Expense(
+      title: "Laptop",
+      amount: 399.99,
+      date: DateTime.now(),
+      category: Category.WORK,
+    ),
+    Expense(
+      title: "Suitcase",
+      amount: 54.99,
+      date: DateTime.now(),
+      category: Category.TRAVEL,
+    ),
+    Expense(
+      title: "Oven",
+      amount: 9.99,
+      date: DateTime.now(),
+      category: Category.FOOD,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
