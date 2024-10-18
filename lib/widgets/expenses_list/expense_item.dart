62c0e835abf2c168e063bf4ace8c2985b9eb5802
Rafly Yanuar <muhammad.rafly02@ui.ac.id>
2024-10-18 20:53:36 +0700

    set up expense item

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -1,12 +1,43 @@
-
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ExpenseItem extends StatelessWidget {
-  const ExpenseItem({super.key});
+  const ExpenseItem({super.key, required this.expense});
+
+  final Expense expense;
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 12,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(expense.title),
+            const SizedBox(
+              height: 4,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('\$${expense.amount.toStringAsFixed(2)}'),
+                Row(
+                  children: [
+                    const Icon(Icons.airplane_ticket),
+                    const SizedBox(width: 6),
+                    Text(DateFormat('dd/MM/yyyy').format(expense.date)),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
