740ffa96f9527e7f41da266503520ad80dedcdba
Rafly Yanuar <muhammad.rafly02@ui.ac.id>
2024-10-18 20:33:17 +0700

    restructured widgets

---
+++ lib/widgets/expenses_list/expense_item.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
